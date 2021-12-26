a50037ac01aafb8413e8353d409b41de2102f62f
Matt <42381590+Magomed18@users.noreply.github.com>
2021-12-26 23:28:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,22 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start_speedtest'])
 def command_handler(message: Message):
     #bot.reply_to(message, 'Получение результата...')
+    
+    try:
+        bot.reply_to(message, 'Получение оптимального сервера')
+        a = test.get_best_server()
+        srvr = a['host'], a['name'], a['country']
+        bot.reply_to(message, srvr)
+    except Exception:
+        bot.reply_to(message, 'Извините, произошла ошибка при получении данных')
+        
     try:
         bot.reply_to(message, 'Получение скорости скачивания...')
         download_result = test.download() / 1024 / 1024
         bot.reply_to(message, f'Donload result: {"%.2f" % download_result}/Mbit.')
     except Exception:
         bot.reply_to(message, 'Извините, произошла ошибка при получении данных')
+    
     try:
         bot.reply_to(message, 'Получение скорости загрузки...')
         upload_result = test.upload() / 1024 / 1024
