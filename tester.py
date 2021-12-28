43523eed3e7652871d03c340d6dead8138a60abc
Matt <42381590+Magomed18@users.noreply.github.com>
2021-12-28 15:01:39 +0300

    Update tester.py

--- tester.py
+++ tester.py
@@ -3,16 +3,8 @@ from hurry.filesize import size
 
 test = speedtest.Speedtest()
 
-#Getting servers
-print('Loading server list...')
-a = test.get_best_server()
-print(a['host'], a['name'], a['country'])
-
 #Getting upload and download speed
 download_result = test.download() / 1024 / 1024
 upload_result = test.upload() / 1024 / 1024
-
-print( "%.2f" % download_result)
-print("%.2f" % upload_result)
 
 
